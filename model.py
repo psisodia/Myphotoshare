46c171bf87661013a7ddc989f127e541d3bcbc46
Sisodia <psisodia@MPKL1288bb8c6.local>
2014-07-31 17:03:27 -0700

    adding new model changes for env variables to look at postgress

--- model.py
+++ model.py
@@ -5,13 +5,26 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
 import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 #import pdb
 
 ENGINE = None
 Session = None
 
-ENGINE = create_engine(os.environ.get("DATABASE_URL", "sqlite:///photodb.db"), echo = True)
+ENGINE = create_engine(os.environ.get("DATABASE_URL", "DATABASE_URL_LOCAL"), echo = True)
 dbsession = scoped_session(sessionmaker(bind = ENGINE,
                                     autocommit = False,
                                   autoflush = False))
