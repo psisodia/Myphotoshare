714cdbb7f85ab459c5ba86187a342dc31c40b454
Sisodia <psisodia@MPKL1288bb8c6.local>
2014-08-14 16:52:11 -0700

    updated all css changes and added upload album link

--- model.py
+++ model.py
@@ -8,16 +8,19 @@ import os
 import psycopg2
 import urlparse
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+# urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL_LOCAL"])
 
+print url
 conn = psycopg2.connect(
-    database=url.path[1:],
+    database=url.path[1:]
     user=url.username,
     password=url.password,
     host=url.hostname,
     port=url.port
 )
+
+
 
 #import pdb
 
